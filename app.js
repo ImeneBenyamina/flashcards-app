f6b4f5f13a8a580b477ac9b6b81a5f4c671255a1
Imene Benyamina <imene.benyamina@univ-tiaret.dz>
2024-10-17 18:19:31 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
-var flashcards = [];
-var currentCardIndex = 0;
+var flashcards = {};
+var currentCardSet = null;
 
+var flashcardContainer = document.getElementById('flashcard-container');
 var questionElement = document.getElementById('question');
 var answerElement = document.getElementById('answer');
 var nextButton = document.getElementById('next');
 var toggleAnswerButton = document.getElementById('toggleAnswer');
 
-// Fetch flashcards using XMLHttpRequest (for older browsers)
+// Fetch flashcards using XMLHttpRequest
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'flashcards.json', true);
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
         if (xhr.status === 200) {
             try {
-                flashcards = JSON.parse(xhr.responseText);
-                showCard();
+                flashcards = JSON.parse(xhr.responseText).sets;
+                displaySets();
             } catch (error) {
-                questionElement.textContent = "Error parsing flashcards.";
                 console.error('Error parsing JSON:', error);
             }
         } else {
-            questionElement.textContent = "Error loading flashcards.";
             console.error('Error loading flashcards:', xhr.status);
         }
     }
 };
 xhr.send();
 
-function showCard() {
-    var currentCard = flashcards[currentCardIndex];
+function displaySets() {
+    for (var setName in flashcards) {
+        var card = document.createElement('div');
+        card.className = 'card';
+        card.textContent = setName;
+        card.onclick = function() {
+            var selectedSetName = this.textContent;
+            selectRandomFlashcard(selectedSetName);
+        };
+        flashcardContainer.appendChild(card);
+    }
+}
+
+function selectRandomFlashcard(setName) {
+    currentCardSet = flashcards[setName];
+    var randomIndex = Math.floor(Math.random() * currentCardSet.length);
+    showCard(randomIndex);
+}
+
+function showCard(index) {
+    var currentCard = currentCardSet[index];
     questionElement.textContent = currentCard.question;
     answerElement.textContent = currentCard.answer;
     answerElement.classList.add('hidden');
 }
 
 nextButton.addEventListener('click', function() {
-    currentCardIndex = (currentCardIndex + 1) % flashcards.length;
-    showCard();
+    var randomIndex = Math.floor(Math.random() * currentCardSet.length);
+    showCard(randomIndex);
 });
 
 toggleAnswerButton.addEventListener('click', function() {
